d8c41a2d79d1dee884de4236fcdb5a9efe8512ae
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-09-13 20:22:39 +0530

    Basic jokes app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch((error) =>{
+      console.log(error);
+      
+    })
+  })
+
+  return (
+   <>
+   <h1>Takla with full Stack For Practice</h1>
+   <p>JOKES: {jokes.length}</p>
+
+  {
+    jokes.map((joke, index) => (
+      <div>{joke.id}
+      <h3>{joke.title}</h3>
+      <p>{joke.content}</p>
+      </div>
+    ))
+  }
+
+   </>
+  )
+}
+
+export default App
